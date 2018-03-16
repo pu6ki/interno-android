e23d3d239647aefe307bddc88b2fb2f4db65f5ed
rrhubenov <rrhubenov@gmail.com>
2018-03-16 10:50:09 +0200

    Category choice added

---
+++ app/src/main/java/hacktues/interno/com/interno/ChooseCategory.java
@@ -0,0 +1,40 @@
+package hacktues.interno.com.interno;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseCategory extends AppCompatActivity {
+
+    Button btnContestant;
+    Button btnRecruiter;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_category);
+        btnContestant = (Button) findViewById(R.id.btn_contestant);
+        btnRecruiter = (Button) findViewById(R.id.btn_recruiter);
+        buttonEventListner();
+    }
+
+    public void buttonEventListner(){
+        btnRecruiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ChooseCategory.this, RegisterRecruiter.class);
+                startActivity(intent);
+            }
+        });
+        btnContestant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ChooseCategory.this, RegisterContestant.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/hacktues/interno/com/interno/RegisterRecruiter.java
@@ -0,0 +1,14 @@
+package hacktues.interno.com.interno;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterRecruiter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_recruiter);
+    }
+}
