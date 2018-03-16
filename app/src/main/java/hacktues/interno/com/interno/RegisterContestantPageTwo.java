d03dc2861ca44a3302ae686efbbb3e77ace08b69
rrhubenov <rrhubenov@gmail.com>
2018-03-16 15:13:28 +0200

    Created full register

---
+++ app/src/main/java/hacktues/interno/com/interno/RegisterContestantPageTwo.java
@@ -0,0 +1,13 @@
+package hacktues.interno.com.interno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterContestantPageTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_contestant_page_two);
+    }
+}
