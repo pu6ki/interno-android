204c1d44478418487194fd89ced605a89482c017
rrhubenov <rrhubenov@gmail.com>
2018-03-16 15:54:03 +0200

    s

--- app/src/main/java/hacktues/interno/com/interno/RegisterContestant.java
+++ app/src/main/java/hacktues/interno/com/interno/RegisterContestant.java
@@ -3,12 +3,29 @@ package hacktues.interno.com.interno;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterContestant extends AppCompatActivity {
+
+    Intent intent;
+    Button regContestantContinueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_contestant);
+        regContestantContinueBtn = (Button) findViewById(R.id.btn_register_contestant_continue);
+        btnListener();
+    }
+
+    public void btnListener(){
+        regContestantContinueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(RegisterContestant.this, RegisterContestantPageTwo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
