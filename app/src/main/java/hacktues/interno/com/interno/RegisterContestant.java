4564a8812dc08e809bedb7c18ff924043edd47a9
July Slavchev <jul.slavhev.tues19@gmail.com>
2018-03-17 12:23:38 +0200

    Register updated

--- app/src/main/java/hacktues/interno/com/interno/RegisterContestant.java
+++ app/src/main/java/hacktues/interno/com/interno/RegisterContestant.java
@@ -19,6 +19,8 @@ public class RegisterContestant extends AppCompatActivity {
         btnListener();
     }
 
+
+
     public void btnListener(){
         regContestantContinueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
