c55feed5e7049546ecfe4cdfd3b323b8ec88e82d
July Slavchev <jul.slavhev.tues19@gmail.com>
2018-03-17 15:42:25 +0200

    Registration fixed

--- app/src/main/java/hacktues/interno/com/interno/RegisterRecruiter.java
+++ app/src/main/java/hacktues/interno/com/interno/RegisterRecruiter.java
@@ -3,12 +3,45 @@ package hacktues.interno.com.interno;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterRecruiter extends AppCompatActivity {
+
+    Button btnRegisterRecruiter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_recruiter);
+        btnRegisterRecruiter = (Button) findViewById(R.id.btn_register_recruiter);
+        btnListener();
+    }
+
+    public void btnListener(){
+        btnRegisterRecruiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            EditText company = (EditText)findViewById(R.id.register_recruiter_company);
+            EditText password = (EditText)findViewById(R.id.register_recruiter_password);
+            EditText repassword = (EditText)findViewById(R.id.register_recruiter_repassword);
+            EditText country = (EditText)findViewById(R.id.register_recruiter_country);
+
+            String companystr = company.getText().toString();
+            String passwordstr = password.getText().toString();
+            String repasswordstr = repassword.getText().toString();
+            String countrystr = country.getText().toString();
+
+            if(!password.equals(repassword)) {
+                //popup message
+                Toast pass_error = Toast.makeText(RegisterRecruiter.this, "Passwords don't match!",
+                Toast.LENGTH_LONG);
+                pass_error.show();
+            }
+            }
+        });
     }
 }
