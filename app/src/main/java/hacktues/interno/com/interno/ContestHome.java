4e36510f66c504140643086f136fdeee40310c84
StefchoG <ste.grozev.tues19@gmail.com>
2018-03-18 01:28:17 +0200

    login

--- app/src/main/java/hacktues/interno/com/interno/ContestHome.java
+++ app/src/main/java/hacktues/interno/com/interno/ContestHome.java
@@ -39,8 +39,6 @@ public class ContestHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contest_home);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
